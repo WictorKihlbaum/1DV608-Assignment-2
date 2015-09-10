aeedbaceefe964b081a707bef19a6482133cde49
WictorKihlbaum <wk222as@student.lnu.se>
2015-09-10 21:13:33 +0200

    test
    test

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,7 +5,7 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = 'TODO, Write servertime here... hej';
 
 		return '<p>' . $timeString . '</p>';
 	}
