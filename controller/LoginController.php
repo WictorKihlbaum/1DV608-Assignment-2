e617658dfeb585099e29e508fd99a8751162f003
WictorKihlbaum <wk222as@student.lnu.se>
2015-09-21 16:42:00 +0200

    try catch update

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -33,8 +33,7 @@ class LoginController {
 
             } catch (Exception $e) {
 
-                $errorMessage = $e -> getMessage();
-                echo $errorMessage;
+                echo 'Caught exception: ', $e -> getMessage(), "\n";
                 //var_dump($e->getMessage());
             }
         }
