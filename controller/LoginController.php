74ca917293bbc8e833b24d049c991d7a35fa6c72
WictorKihlbaum <wk222as@student.lnu.se>
2015-09-23 12:24:27 +0200

    some of my old code added

---
+++ controller/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: wk222as
+ * Date: 2015-09-23
+ * Time: 12:23
+ */
+class LoginController {
+
+    private $loginView;
+    private $loginModel;
+
+
+    public function __construct($loginView, $loginModel) {
+
+        $this -> loginView = $loginView;
+        $this -> loginModel = $loginModel;
+    }
+
+    public function loginUser() {
+        // Get user input if user pressed 'login'.
+        if ($this -> loginView -> didUserPressLogin()) {
+
+            try {
+
+                $user = $this -> loginView -> getUser();
+                // Validate user's username/password in 'loginModel' and return true/false.
+                return $this -> loginModel -> validateUserInput($user);
+
+            } catch (\Exception $e) {
+
+                echo "Caught exception: " . $e -> getMessage() . "\n";
+            }
+        }
+    }
+}
